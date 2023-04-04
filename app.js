67d97d4ec6f9f8463c55106a3719050d98eea4b6
AJ ONeal <coolaj86@gmail.com>
2023-04-04 00:09:04 -0600

    feat: submit email to API

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 (function () {
   "use strict";
 
-  let formSuccess = false;
-
   //@ts-ignore
   document
     .querySelector("[name=signup]")
@@ -12,19 +10,80 @@
       let target = event.target;
 
       event.preventDefault();
+      document.querySelector("button")?.setAttribute("disabled", "disabled");
       document.querySelector(".signup-msg")?.remove();
 
-      console.log("do something on form submit", formSuccess);
+      /** @type {HTMLFormElement} */
+      //@ts-ignore
+      let $email = document.querySelector("[type='email']");
+      let email = $email?.value || "";
+      email = email.trim();
+      email = email.toLowerCase();
 
-      let msg = `<h3 class="signup-msg warn">Whoops, looks like something went wrong</h3>`;
-
-      if (formSuccess) {
-        msg = `<h3 class="signup-msg good">You're good to go and should receive a confirmation email shortly.</h3>`;
-        target.querySelector("fieldset")?.setAttribute("disabled", "disabled");
+      let host = location.host || "localhost";
+      if ("localhost" !== host) {
+        host = `api.${host}`;
       }
 
-      target.insertAdjacentHTML("beforeend", msg);
+      let url = `https://${host}/api/request-invite`;
+      let request = { email };
+      let body = JSON.stringify(request, null, 2);
 
-      formSuccess = !formSuccess;
+      window
+        .fetch(url, {
+          method: "POST",
+          mode: "cors",
+          headers: { "Content-Type": "application/json" },
+          body,
+        })
+        .then(mustOk)
+        .then(showSuccess)
+        .catch(showFailure);
+
+      async function showSuccess() {
+        //let msg = `You're good to go and should receive a confirmation email shortly.`;
+        let msg = `You're good to go. We'll reach out shortly.`;
+        let msgHtml = `<h3 class="signup-msg good">${msg}</h3>`;
+        target.querySelector("fieldset")?.setAttribute("disabled", "disabled");
+        target.insertAdjacentHTML("beforeend", msgHtml);
+      }
+
+      /** @param {Error} err */
+      async function showFailure(err) {
+        console.error(`Failed to send request for invite`);
+        console.error(err);
+
+        let msgHtml = `<h3 class="signup-msg warn">Whoops, looks like something went wrong. <br/>Email <a href="mailto:bnnadotnet@therootcompany.com">bnnadotnet@therootcompany.com</a> instead.</h3>`;
+        target.insertAdjacentHTML("beforeend", msgHtml);
+      }
     });
+
+  /** @param {Response} resp */
+  async function mustOk(resp) {
+    let body = await resp.text();
+    let result;
+
+    try {
+      result = JSON.parse(body);
+    } catch (e) {
+      //@ts-ignore
+      e.code = "E_PARSE";
+      //@ts-ignore
+      e.body = body;
+      //@ts-ignore
+      e.response = resp;
+      throw e;
+    }
+
+    if (!resp.ok) {
+      let err = new Error(`did not get 200 OK`);
+      //@ts-ignore
+      err.code = "E_NOT_OK";
+      //@ts-ignore
+      err.result = result;
+      //@ts-ignore
+      err.response = resp;
+      throw err;
+    }
+  }
 })();
