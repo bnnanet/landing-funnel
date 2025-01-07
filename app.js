3fbb087bfa16d450088985cdf1ba6e48f33ebdfd
AJ ONeal <aj@therootcompany.com>
2025-01-07 16:41:34 -0700

    chore: add poor man's captcha

--- app.js
+++ app.js
@@ -13,12 +13,15 @@
       document.querySelector("button")?.setAttribute("disabled", "disabled");
       document.querySelector(".signup-msg")?.remove();
 
-      /** @type {HTMLFormElement} */
-      //@ts-ignore
+      /** @type {HTMLFormElement} */ //@ts-expect-error
       let $email = document.querySelector("[type='email']");
-      let email = $email?.value || "";
+      let email = $email.value || "";
       email = email.trim();
       email = email.toLowerCase();
+
+      /** @type {HTMLFormElement} */ //@ts-expect-error
+      let $csrfToken = document.querySelector("[name='csrf-token']");
+      let csrfToken = $csrfToken.value || "";
 
       let host = location.host || "localhost";
       if ("localhost" !== host) {
@@ -26,33 +29,40 @@
       }
 
       let url = `https://${host}/api/request-invite`;
-      let request = { email };
+      let request = { email: email, "csrf-token": csrfToken };
       let body = JSON.stringify(request, null, 2);
 
-      window
-        .fetch(url, {
-          method: "POST",
-          mode: "cors",
-          headers: { "Content-Type": "application/json" },
-          body,
-        })
-        .then(mustOk)
-        .then(showSuccess)
-        .catch(showFailure);
+      let p;
+      let yes = window.confirm(
+        `Thanks, we'll send an automated message to confirm your email address.`,
+      );
+      if (yes) {
+        p = window
+          .fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: { "Content-Type": "application/json" },
+            body,
+          })
+          .then(mustOk);
+      } else {
+        p = Promise.resolve();
+      }
+      p.then(showSuccess).catch(showFailure);
 
       /**
        * @param {string} msg
        * @param {string} [cls]
        */
-      function msgHtml(msg, cls = '') {
-        return `<h3 class="${['signup-msg', cls].join(' ')}">${msg}</h3>`
+      function msgHtml(msg, cls = "") {
+        return `<h3 class="${["signup-msg", cls].join(" ")}">${msg}</h3>`;
       }
 
       async function showSuccess() {
         //let msg = `You're good to go and should receive a confirmation email shortly.`;
         let msg = `You're good to go. We'll reach out shortly.`;
         target.querySelector("fieldset")?.setAttribute("disabled", "disabled");
-        target.insertAdjacentHTML("beforeend", msgHtml(msg, 'good'));
+        target.insertAdjacentHTML("beforeend", msgHtml(msg, "good"));
       }
 
       /** @param {Error} err */
@@ -60,10 +70,10 @@
         console.error(`Failed to send request for invite`);
         console.error(err);
 
-        let email = 'bnnadotnet@therootcompany.com'
+        let email = "bnnadotnet@therootcompany.com";
         let msg = `Whoops, looks like something went wrong.<br/>
           Email <a href="mailto:${email}">${email}</a> instead.`;
-        target.insertAdjacentHTML("beforeend", msgHtml(msg, 'warn'));
+        target.insertAdjacentHTML("beforeend", msgHtml(msg, "warn"));
       }
     });
 
